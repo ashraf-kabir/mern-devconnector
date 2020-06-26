5223226dfc778a168bfdb3cf1b038b0e36db0690
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 19:50:41 +0600

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,7 +139,7 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
-// @route     GET api/profile
+// @route     DELETE api/profile
 // @desc      delete profile, user & posts
 // @access    Private
 router.delete('/', auth, async (req, res) => {
@@ -155,5 +155,60 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route     PUT api/profile/experience
+// @desc      Add profile experience
+// @access    Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+      
+    }
+  }
+);
 
 module.exports = router;
