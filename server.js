d89ab85554fcaaf3d03cd125b1ae7e5de1029b65
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 13:53:52 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// connect database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
