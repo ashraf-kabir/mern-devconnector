1bcb5ba7a9a580001ebfe8b44d05553121186d14
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 19:33:44 +0600

    delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route     GET api/profile
+// @desc      delete profile, user & posts
+// @access    Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
