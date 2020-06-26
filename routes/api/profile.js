28e756baf1730b9f796e1b6722916c263a95a7ed
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 21:21:06 +0600

    get github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
+const axios = require('axios')
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
@@ -308,6 +311,28 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
+  }
+});
+
+// @route     GET api/profile/github/:username
+// @desc      get user repos from github
+// @access    Public
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No Github profile found' });
   }
 });
 
