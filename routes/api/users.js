8630559e9c5ba9193f014b46f3c85240d57c186c
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 17:32:57 +0600

    user auth login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -34,7 +34,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // get users gravatar
