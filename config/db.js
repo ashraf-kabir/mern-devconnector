b94008e49cd2dc919731afd92a2d15192edb0705
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-26 18:54:20 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,20 +3,20 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-      console.log('MongoDB connected...');
-    } catch(err) {
-      console.error(err.message);
-      // exit process with failure
-      process.exit(1);
-    }
-}
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
