0dd345dda7ae3aed64b63fef98c56334e89ef07a
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-29 17:41:42 +0600

    addressing console warnings

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -234,7 +234,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm("Are you sure? This CAN'T be UNDONE!")) {
     try {
-      const res = await axios.delete('/api/profile');
+      await axios.delete('/api/profile');
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -125,4 +125,4 @@ AddEducation.propTypes = {
   addEducation: PropTypes.func.isRequired,
 };
 
-export default connect(null, { addEducation })(AddEducation);
+export default connect(null, { addEducation })(withRouter(AddEducation));
