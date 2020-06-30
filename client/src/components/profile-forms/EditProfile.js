2e5cf74e58d9730f71565ea90f22d9762843bcb4
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-30 14:57:15 +0600

    Update EditProfile.js

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -71,7 +71,7 @@ const EditProfile = ({
   };
   return (
     <Fragment>
-      <h1 className='large text-primary'>Create Your Profile</h1>
+      <h1 className='large text-primary'>Edit Your Profile</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Let's get some information to make your
         profile stand out
